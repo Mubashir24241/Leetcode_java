25e0500d27c2549817e45cb145788bb486bf675f
Mubashir24241 <mubashir.ahmad.cse.2020@miet.ac.in>
2024-11-13 12:22:25 +0530

    2563. Count the Number of Fair Pairs

--- strings/2563. Count the Number of Fair Pairs.java
+++ strings/2563. Count the Number of Fair Pairs.java
@@ -15,3 +15,8 @@ class Solution {
       return res;
     }
   }
+
+// First, it sorts nums to use an efficient two-pointer approach. 
+// The function countFairPairs then calculates pairs with sums up to upper and subtracts those with sums below lower, getting the count within [lower, upper].
+// The helper function countLess uses two pointers to count pairs: for each element i, it finds the farthest element j to the right where nums[i] + nums[j] is within the target sum. 
+// giving a total time complexity of 𝑂(𝑛log𝑛)
